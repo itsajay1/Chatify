b7aa3570cb658db62dbc6baad499f9d85cd6c784
Ajay <technet189@gmail.com>
2026-03-03 08:33:54 +0545

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,11 +16,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 // make ready for deployment
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
     });
 }
 
