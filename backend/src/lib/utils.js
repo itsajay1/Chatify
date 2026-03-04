0afeab698c8ea5e69276c88b41b01e8365a15c2e
Ajay <technet189@gmail.com>
2026-03-04 15:59:49 +0545

    Welcome email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-    const { JWT_SECRET} = process.env;
+    const { JWT_SECRET} = ENV;
     if (!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
     }
@@ -15,7 +16,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true, // not accessible via JavaScript, helps prevent XSS attacks : cross-site scripting attacks
     sameSite: "strict", // only send cookie for same site requests, helps prevent CSRF attacks : cross-site request forgery attacks
-    secure: process.env.NODE_ENV === "development" ? false : true, // only send cookie over HTTPS in production
+    secure: ENV.NODE_ENV === "development" ? false : true, // only send cookie over HTTPS in production
   });
   return token;
 };
