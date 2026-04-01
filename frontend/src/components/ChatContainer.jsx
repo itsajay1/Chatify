710b26bcdc8dc931be04782de72ea8865636f3cc
Ajay <technet189@gmail.com>
2026-04-01 23:05:48 +0545

    realtime messages implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -20,6 +20,10 @@ function ChatContainer() {
 
   useEffect(() => {
     getMessagesByUserId(selectedUser._id);
+    subscribeToMessages();
+
+    //clean up
+    return () => unsubscribeFromMessages();
 
   }, [selectedUser, getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages]);
 
